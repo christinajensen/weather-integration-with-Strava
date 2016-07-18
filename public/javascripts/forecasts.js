12c3071f5014e43386017f785ba9ca1af84804aa
Christina Jensen <cj.christina.jensen@gmail.com>
2016-07-17 22:50:06 -0700

    adding weather icons to map

--- public/javascripts/forecasts.js
+++ public/javascripts/forecasts.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
   var map;
   var myLatLng;
+  var segments;
+  var iconCode;
+  var weatherIcon;
+  var styles;
+  var marker;
 
+  // Ajax call to Strava API
   $.ajax({
     method: "GET",
-    url: "https://www.strava.com/api/v3/routes/5775019?access_token=758e69afaa0c7dd7395146ca02b1dc51d3c24880", 
+    url: "https://www.strava.com/api/v3/routes/5775778?access_token=758e69afaa0c7dd7395146ca02b1dc51d3c24880", 
     dataType: 'jsonp'
   })
   .done(function(data) {
-    myLatLng = {};
-    console.log(data.segments);
-    myLatLng['lat'] = data.segments[30].end_latlng[0];
-    myLatLng['lng'] = data.segments[30].end_latlng[1];
-
-    // add google map to '/forecasts'
+    // add google map
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 12
+        center: {lat: 37.7749295, lng: -122.4194155},
+        zoom: 10
       });
 
       // change map color+style
-      var styles = [{
+      styles = [{
         "featureType": "all",
         "elementType": "all",
         "stylers": [{
@@ -33,39 +34,49 @@ $(document).ready(function() {
       }];
       map.setOptions({styles: styles});
     }
-
     initMap();
 
-    // change marker color
-    var pinColor = "FFA500";
-    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0,0),
-      new google.maps.Point(10, 34));
+    // add marker as a weather icon
+    segments = data.segments;
+    myLatLng = [];
 
-    // add marker
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: pinImage
+    for (var i = 0; i < segments.length; i++) {
+      if (i && i % 15 === 0) {
+        myLatLng.push({lat: data.segments[i].end_latlng[0], lng: data.segments[i].end_latlng[1]});
+      }
+    }
+
+    $.each(myLatLng, function(i, value) {
+      // AJAX call to weather API
+      $.ajax({
+        method: "GET",
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + value.lat + "&lon=" + value.lng + "&weather=icon&APPID=12fa1a4d7a570fc0ffd5c2895988482a", 
+        dataType: 'jsonp'
+      })
+      .done(function(data) {
+        iconCode = data.weather[0].icon;
+        createWeatherIcon(iconCode, value);
+      })
+      .fail(function(err){
+        console.log("FAIL", err)
+      }); 
+
+    function createWeatherIcon(iconCode, value) {
+      weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      marker = new google.maps.Marker({
+        position: {lat: value.lat, lng: value.lng},
+        map: map,
+        icon: weatherIcon
+      }); 
+    }
+
     });
+  
 
   })
   .fail(function(err){
-    console.log("FAIL")
-    console.log(err)
+    console.log("FAIL", err)
   });
 
-
-
-  // add marker on click
-  // var mapDiv = document.getElementById('map');
-  // google.maps.event.addListener(map, 'click', addMarker);
-
-  // function addMarker(event) {
-    // var lat = event.latLng.lat();
-    // var lng = event.latLng.lng();
-  // }
-  
 
 });
